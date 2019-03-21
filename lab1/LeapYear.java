ac3fcb0747d25874d83baab62531ca68fe53397b
ghw <guohaowei11@gmail.com>
2019-03-21 16:15:20 +0800

    complited first part of lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,3 +1,5 @@
+package lab1;
+
 /** Class that determines whether or not a year is a leap year.
  *  @author YOUR NAME HERE
  */
@@ -12,6 +14,10 @@ public class LeapYear {
         } else {
             System.out.printf("%d is not a leap year.\n", year);
         }
+    }
+
+    private static boolean isLeapYear(int year) {
+        return false;
     }
 
     /** Must be provided an integer as a command line argument ARGS. */
